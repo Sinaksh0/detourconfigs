d1ec9ff652c3d4e195323e5857303933177be6c2
SINA <shrqysyna@gmail.com>
2025-10-04 12:44:56 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 pkg update -y && pkg install -y python
 
 # گرفتن فایل پایتون
-curl -fsSL https://raw.githubusercontent.com/SinaDev/DetourInstaller/main/config_manager.py -o config_manager.py
+curl -fsSL https://raw.githubusercontent.com/Sinaksh0/detourconfigs/refs/heads/main/config.manager.py -o config_manager.py
 
 # اجرای فایل
 python config_manager.py
 
 # انتقال خروجی به حافظه گوشی
-cp ss_configs $HOME/storage/downloads/ && echo "✅ فایل منتقل شد به Downloads"
+cp configs $HOME/storage/downloads/ && echo "✅ فایل منتقل شد به Downloads"
