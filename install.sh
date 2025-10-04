2176993c4a1106013cf2ed3572d5cf9ad41b8bd4
SINA <shrqysyna@gmail.com>
2025-10-04 15:09:45 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # نصب پیش‌نیازها
 pkg update -y && pkg install -y python
 
+#دسترسی به حافظه
+termux-setup-storage
+
 # گرفتن فایل پایتون
 curl -fsSL https://raw.githubusercontent.com/Sinaksh0/detourconfigs/refs/heads/main/config.manager.py -o config_manager.py
 
